e8609ca9d9e3261fb9008c3b88a0e2c9f3058ddc
unknown <naishar.shah31@gmail.com>
2021-01-11 19:47:20 +0530

    Basic Calculator

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,10 +6,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    Spinner spinner;
+    EditText number1_input;
+    EditText number2_input;
+    TextView answer;
 
 
     @Override
@@ -17,18 +27,64 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner operationSpinner = findViewById(R.id.operation);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.operation, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        operationSpinner.setAdapter(adapter);
-        operationSpinner.setOnItemClickListener(this);
+        spinner = (Spinner) findViewById(R.id.option_spinner);
+        spinner.setOnItemSelectedListener(this);
+
+        number1_input   = (EditText)findViewById(R.id.number1);
+        number2_input   = (EditText)findViewById(R.id.number2);
+        answer = (TextView) findViewById(R.id.answer);
+
+
+
+        List<String> options = new ArrayList<String>();
+        options.add("+");
+        options.add("-");
+        options.add("*");
+        options.add("/");
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, options);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+
+
+
+
 
     }
 
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        //String item = parent.getItemAtPosition(position).toString();
+        //Toast.makeText(parent.getContext(), "Selected: " + item, Toast.LENGTH_LONG).show();
+        // TODO Auto-generated method stub
+
+    }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        String optionOperation = parent.getItemAtPosition(position).toString();
-        Toast.makeText(this, optionOperation, Toast.LENGTH_LONG).show();
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
+    }
+
+    public void calculate(View view) {
+
+        String option = spinner.getSelectedItem().toString();
+        String number1 = number1_input.getText().toString();
+        String number2 = number2_input.getText().toString();
+
+        /*Toast toast = Toast.makeText(this, (number1 + number2),
+                Toast.LENGTH_LONG);
+
+        toast.show();*/
+
+        //VALIDATION
+        if ( number1.matches("") || number2.matches("") ) {
+            Toast toast = Toast.makeText(this, "Please enter both values",
+                    Toast.LENGTH_LONG);
+            toast.show();
+        } else {
+
+        }
+
+
     }
 }
